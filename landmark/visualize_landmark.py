d9a905f0cb9a4b74bc2ab1cd213332c97b9f4520
priyash7 <priyashshah4@gmail.com>
2024-10-30 14:45:52 +0530

    minor changes

--- landmark/visualize_landmark.py
+++ landmark/visualize_landmark.py
@@ -10,7 +10,7 @@ face_mesh = mp_face_mesh.FaceMesh()
 # landmark_indices = [1,168,205,425,10,190,414,162,389,152]
 
 landmark_indices = [15, 16, 17, 18, 19, 33, 44, 62, 63, 77, 78, 84, 85, 86, 87, 88, 89, 90, 91, 92, 96, 97, 107, 141, 147, 149, 150, 153, 171, 172, 176, 177, 179, 180, 181, 182, 183, 195, 200, 201, 202, 205, 209, 212, 263, 308, 314, 315, 316, 317, 318, 319, 320, 321, 322, 325, 326, 336, 370, 376, 378, 379, 396, 397, 401, 403, 404, 405, 406, 407, 419, 422, 425, 429, 432]
-image_path = 'radboud_nut.jpg'  # Provide the path to your image
+image_path = 'radboud_nut_sample.jpg'  # Provide the path to your image
 image = cv2.imread(image_path)
 
 # Convert BGR image to RGB for processing
